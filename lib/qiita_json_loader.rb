664d98f73d2ca2cb1368fc5e9e3014afd43d3fce
tbpgr <tbpgr@tbpgr.jp>
2014-06-28 00:27:08 +0900

    implement load QiitaAPI JSON

--- lib/qiita_json_loader.rb
+++ lib/qiita_json_loader.rb
@@ -1,20 +1,43 @@
 require 'open-uri'
 require 'openssl'
 require 'json'
+require 'models/article'
+require 'models/articles'
 
 module QiitaMatome
   # QiitaMatome::QiitaJsonLoader
   class QiitaJsonLoader
     attr_reader :user, :articles
+    QIITA_URL = 'https://qiita.com/api/v1/users/%s/items?page=%s&per_page=%s'
+    PER_PAGE = 25
+    PAGE_LIMIT = 100
+
+    def initialize
+      @articles = Articles.new
+    end
+
     def load(user)
       @user = user
       tmp_verbose = $VERBOSE
       $VERBOSE = nil
       OpenSSL::SSL.const_set('VERIFY_PEER', OpenSSL::SSL::VERIFY_NONE)
       $VERBOSE = tmp_verbose
-      html = open("https://qiita.com/api/v1/users/#{user}/items").read
-      articles = JSON.parser.new(html)
-      @articles = articles.parse
+      (1..PAGE_LIMIT).each do |page|
+        before_size = @articles.size
+        load_page(user, page)
+        break if before_size == @articles.size
+      end
+    end
+
+    private
+
+    def load_page(user, page)
+      json = open(format(QIITA_URL, user, page, PER_PAGE)).read
+      json_articles = JSON.parser.new(json).parse
+      @articles += json_articles.each_with_object(Articles.new) do |item, memo|
+        memo << Articles.new(item)
+        memo
+      end
     end
   end
 end
