5490d0a9066a204f120ea825b914332952ced82e
tbpgr <tbpgr@tbpgr.jp>
2014-06-30 00:06:27 +0900

    add created_at_ymdhms/updated_at_ymdhms methods to Article

--- lib/models/article.rb
+++ lib/models/article.rb
@@ -3,6 +3,7 @@ require 'date'
 module QiitaMatome
   # QiitaMatome::Article
   class Article
+    YMDHMS_DATETIME_FORMAT = '%Y/%m/%d %H:%M:%S'.freeze
     attr_accessor :user, :title, :uuid, :created_at, :updated_at, :tags, :stock_count # rubocop:disable LineLength
 
     def initialize(options = {})
@@ -10,10 +11,18 @@ module QiitaMatome
       @title = options[:title]
       @uuid = options[:uuid]
       @created_at = DateTime.parse(options[:created_at]) unless options[:created_at].nil?
-      @updated_at = DateTime.parse(options[:updated_at]) unless options[:created_at].nil?
+      @updated_at = DateTime.parse(options[:updated_at]) unless options[:updated_at].nil?
       @tags = options[:tags]
       @stock_count = options[:stock_count]
       yield(self) if block_given?
+    end
+
+    def created_at_ymdhms
+      @created_at.strftime(YMDHMS_DATETIME_FORMAT)
+    end
+
+    def updated_at_ymdhms
+      @updated_at.strftime(YMDHMS_DATETIME_FORMAT)
     end
   end
 end
